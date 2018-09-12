99c6b454c19240e5cdfdf7dc229c963087375ec8
yjs2952 <yjs2952@naver.com>
2018-09-12 12:40:34 +0900

    TotalFileLength 수정

--- src/examples/io/TotalFileLength.java
+++ src/examples/io/TotalFileLength.java
@@ -5,25 +5,25 @@ import java.io.File;
 public class TotalFileLength {
     public static void main(String[] args) {
         File file = new File(".");
-        System.out.println(file.getAbsoluteFile());
-
         File[] files = file.listFiles();
-        System.out.println(fileLength(files));
 
+        if (files.length != 0) {
+            System.out.println(fileLength(files));
+        } else {
+            System.out.println("no file exists");
+        }
     }
 
-    public static long fileLength(File[] files){
+    public static long fileLength(File[] files) {
 
         long sum = 0;
 
-        if (files.length != 0) {
-            for (File f : files) {
-                if (f.isDirectory()) {
-                    File[] subFiles = f.listFiles();
-                    sum += fileLength(subFiles);
-                } else {
-                    sum += f.length();
-                }
+        for (File f : files) {
+            if (f.isDirectory()) {
+                File[] subFiles = f.listFiles();
+                sum += fileLength(subFiles);
+            } else {
+                sum += f.length();
             }
         }
 
